b02d00ca072b56abee0d53b89875ee63134c5a0c
Werner Strauch <werner.strauch@gmail.com>
2023-08-04 16:50:58 +0200

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,25 @@
 import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import tailwind from "@astrojs/tailwind";
+import basicSsl from '@vitejs/plugin-basic-ssl';
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      page: 'storyblok/Page',
+      feature: 'storyblok/Feature',
+      grid: 'storyblok/Grid',
+      teaser: 'storyblok/Teaser',
+    }
+  }), tailwind()],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true
+    }
+  }
+});
